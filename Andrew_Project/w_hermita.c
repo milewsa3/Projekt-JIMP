09d5465b2996a7085eda3bd88a55ff3890fa5e26
milewsa3 <a.milewski@onet.eu>
2020-01-12 14:53:51 +0100

    Usun prosta z hermita

--- Andrew_Project/w_hermita.c
+++ Andrew_Project/w_hermita.c
@@ -6,18 +6,5 @@
 
 void make_spl(points_t * pts, spline_t * spl)
 {
-
-if (pts->n < 2 || alloc_spl (spl, 1)) {
-    spl->n = 0;
-    return;
-  }
-  else {
-    spl->x[0] = pts->x[0];
-    spl->f[0] = pts->y[0];
-    spl->f1[0] =
-      (pts->y[pts->n - 1] - pts->y[0]) / (pts->x[pts->n - 1] - pts->x[0]); //to wzor na a czyli y2-y1/x2-x1
-    spl->f2[0] = spl->f3[0] = 0;
-    spl->n = 1;
-  }
-		
+	
 }	
